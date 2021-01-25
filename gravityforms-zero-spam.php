2cbab8cc61e72b71bbccbe5939394b50571fc494
Zack Katz <zack@katz.co>
2021-01-25 14:16:11 -0500

    Version 1.0.4
    The final version created and published by GoWP.

---
+++ gravityforms-zero-spam.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Plugin Name:       Gravity Forms Zero Spam
+ * Plugin URI:        http://www.gowp.com/plugins/gravityforms-zero-spam
+ * Description:       Enhance your Gravity Forms to include anti-spam measures originally based on the work of David Walsh's <a href="http://davidwalsh.name/wordpress-comment-spam">"Zero Spam"</a> technique.
+ * Version:           1.0.4
+ * Author:            GoWP
+ * Author URI:        https://www.gowp.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// my mother always said to use things as they're intended or not at all
+
+	if ( ! defined( 'WPINC' ) ) {
+		die;
+	}
+
+// clean up after ourselves
+
+	register_deactivation_hook( __FILE__, array( 'GF_Zero_Spam', 'deactivate' ) );
+
+// main plugin class
+
+	class GF_Zero_Spam {
+		public function __construct() { // instantiation (is that a word?)
+			add_action( 'wp_footer', array( $this, 'add_key_field' ), 9999 ); // add key injection JS to the bottom of the page
+			add_filter( 'gform_entry_is_spam', array( $this, 'check_key_field' ) ); // add our validation check to all forms
+		}
+		public function deactivate() { // plugin deactivation
+			delete_option( 'gf_zero_spam_key' ); // remove the key
+		}
+		public function get_key() { // retrieve they key, generating if needed
+			if ( ! $key = get_option( 'gf_zero_spam_key' ) ) {
+				$key = wp_generate_password( 64, false, false );
+				update_option( 'gf_zero_spam_key', $key, FALSE );
+			}
+			return $key;
+		}
+		public function add_key_field( $form ) { // inject the hidden field and key into the form at submission
+			?>
+			<script type='text/javascript'>
+				jQuery(document).ready(function($){
+					var gforms = '.gform_wrapper form';
+					$( document ).on( 'submit', gforms ,function() {
+						$('<input>').attr( 'type', 'hidden' )
+								.attr( 'name', 'gf_zero_spam_key' )
+								.attr( 'value', '<?php echo $this->get_key(); ?>' )
+								.appendTo( gforms );
+						return true;
+					});
+				});
+			</script>
+			<?php
+		}
+		public function check_key_field( $is_spam ) { // check for the key during validation
+			if ( ! isset( $_POST['gf_zero_spam_key'] ) || ( $_POST['gf_zero_spam_key'] != $this->get_key() ) ) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+// Fire it up
+
+	$gf_zero_spam = new GF_Zero_Spam;
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Gravity Forms Zero Spam ===
+Contributors: karpstrucking, supporthero
+Tags: gravityforms, gravity forms, anti-spam, antispam, spam, spam-blocker, spambot, spammer, addons, add-ons
+Requires at least: 3.0.1
+Tested up to: 5.5.1
+Stable tag: 1.0.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enhance your Gravity Forms to include anti-spam measures originally based on the work of David Walsh's "Zero Spam" technique.
+
+== Description ==
+This plugin adds a non-obtrusive anti-spam measure to all of your Gravity Forms. This measure is originally based on David Walsh's "Zero Spam" technique.
+
+Requires: Gravity Forms v1.5+
+
+== Frequently Asked Questions ==
+= Is this plugin PHP7 compatible? =
+ 
+Yes.
+
+= Will this block spam comments or registrations on my site? =
+
+No. For that, we recommend Ben Marshall‘s [WordPress Zero Spam plugin](https://wordpress.org/plugins/zero-spam/).
+
+== Changelog ==
+= 1.0.4 =
+* Should fix 'jQuery undefined' errors
+
+= 1.0.3 =
+* Added support for multi-page forms (thanks, @supporthero)
+
+= 1.0.2 =
+* Update FAQ and compatibility notes in readme.txt
+
+= 1.0.1 =
+* Fix issue with forms not submitting after an initial validation error
+
+= 1.0.0 =
+* Initial version
