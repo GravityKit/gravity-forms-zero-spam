43dd6f1306d18b50aaf735c0da043dabe5f00a7a
Malay Ladu <malayladu@gmail.com>
2023-11-10 15:59:56 +0530

    Update docblock

--- gravityforms-zero-spam.php
+++ gravityforms-zero-spam.php
@@ -10,26 +10,25 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-// my mother always said to use things as they're intended or not at all
+// my mother always said to use things as they're intended or not at all.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
 define( 'GF_ZERO_SPAM_BASENAME', plugin_basename( __FILE__ ) );
 
-// clean up after ourselves
+// clean up after ourselves.
 register_deactivation_hook( __FILE__, array( 'GF_Zero_Spam', 'deactivate' ) );
 
-// Fire it up
+// Fire it up.
 add_action( 'gform_loaded', array( 'GF_Zero_Spam', 'gform_loaded' ) );
 
 class GF_Zero_Spam {
 
 	/**
-	 * Instantiate the plugin on Gravity Forms loading
+	 * Instantiate the plugin on Gravity Forms loading.
 	 */
 	public static function gform_loaded() {
-
 		include_once plugin_dir_path( __FILE__ ) . 'gravityforms-zero-spam-form-settings.php';
 
 		new self;
@@ -80,12 +79,11 @@ class GF_Zero_Spam {
 	}
 
 	/**
-	 * Retrieves the zero spam key (generating if needed)
+	 * Retrieves the zero spam key (generating if needed).
 	 *
 	 * @return false|mixed|void
 	 */
 	public function get_key() {
-
 		$key = get_option( 'gf_zero_spam_key' );
 
 		if ( ! $key ) {
@@ -97,19 +95,20 @@ class GF_Zero_Spam {
 	}
 
 	/**
-	 * Injects the hidden field and key into the form at submission
+	 * Injects the hidden field and key into the form at submission.
 	 *
 	 * @uses GFFormDisplay::add_init_script() to inject the code into the `gform_post_render` jQuery hook.
 	 *
-	 * @param array $form The Form Object
+	 * @param array $form The Form Object.
 	 *
 	 * @return void
 	 */
 	public function add_key_field( $form ) {
-
 		/**
 		 * Allows the zero spam key field to be disabled by returning false.
+		 *
 		 * @since 1.4
+		 *
 		 * @param bool $add_key_field Whether to add the key field to the form. Default true.
 		 */
 		$add_key_field = apply_filters( 'gf_zero_spam_add_key_field', true );
@@ -136,11 +135,11 @@ EOD;
 	}
 
 	/**
-	 * Checks for our zero spam key during validation
+	 * Checks for our zero spam key during validation.
 	 *
-	 * @param bool $is_spam Indicates if the submission has been flagged as spam.
-	 * @param array $form The form currently being processed.
-	 * @param array $entry The entry currently being processed.
+	 * @param bool  $is_spam Indicates if the submission has been flagged as spam.
+	 * @param array $form    The form currently being processed.
+	 * @param array $entry   The entry currently being processed.
 	 *
 	 * @return bool True: it's spam; False: it's not spam!
 	 */
@@ -153,9 +152,9 @@ EOD;
 		 *
 		 * @since 1.2
 		 *
-		 * @param bool $should_check_key_field Whether the Zero Spam plugin should check for the existence and validity of the key field. Default: true.
-		 * @param array $form The form currently being processed.
-		 * @param array $entry The entry currently being processed.
+		 * @param bool  $should_check_key_field Whether the Zero Spam plugin should check for the existence and validity of the key field. Default: true.
+		 * @param array $form                   The form currently being processed.
+		 * @param array $entry                  The entry currently being processed.
 		 */
 		$should_check_key_field = gf_apply_filters( 'gf_zero_spam_check_key_field', rgar( $form, 'id' ), $should_check_key_field, $form, $entry );
 
@@ -168,12 +167,12 @@ EOD;
 			return $is_spam;
 		}
 
-	    // This was not submitted using a web form; created using API
+	    // This was not submitted using a web form; created using API.
 		if ( ! $supports_context && ! did_action( 'gform_pre_submission' ) ) {
 			return $is_spam;
 		}
 
-		// Created using REST API or GFAPI
+		// Created using REST API or GFAPI.
 		if ( isset( $entry['user_agent'] ) && 'API' === $entry['user_agent'] ) {
 			return $is_spam;
 		}
@@ -195,7 +194,6 @@ EOD;
 	 * @param array $entry The entry data.
 	 */
 	public function add_entry_note( $entry ) {
-
 		if ( 'spam' !== rgar( $entry, 'status' ) ) {
 			return;
 		}
@@ -206,5 +204,4 @@ EOD;
 
 		GFAPI::add_note( $entry['id'], 0, 'Zero Spam', __( 'This entry has been marked as spam.', 'gf-zero-spam' ), 'gf-zero-spam', 'success' );
 	}
-
 }
